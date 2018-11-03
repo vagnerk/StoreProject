a3887f1d6eabf5324e8010a3d8486a969982912f
Vagner Ferreira <vagner.k.ferreira@hotmail.com>
2018-11-03 09:43:10 +0100

    Add project files.

---
+++ BabyStore/ViewModels/ProductIndexViewModel.cs
@@ -0,0 +1,42 @@
+﻿using BabyStore.Models;
+using PagedList;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace BabyStore.ViewModels
+{
+	public class ProductIndexViewModel
+	{
+		public IPagedList<Product> Products { get; set; }
+		public string Search { get; set; }
+		public IEnumerable<CategoryWithCount> CatsWithCount { get; set; }
+		public string Category { get; set; }
+		public string SortBy { get; set; }
+		public Dictionary<string, string> Sorts { get; set; }
+		public IEnumerable<SelectListItem> CatFilterItems
+		{
+			get
+			{
+				var allCats = CatsWithCount.Select(cc => new SelectListItem
+				{
+					Value = cc.CategoryName,
+					Text = cc.CatNameWithCount
+				});
+				return allCats;
+			}
+		}
+	}
+	public class CategoryWithCount
+	{
+		public int ProductCount { get; set; }
+		public string CategoryName { get; set; }
+		public string CatNameWithCount
+		{
+			get
+			{
+				return CategoryName + " (" + ProductCount.ToString() + ")";
+			}
+		}
+	}
+}
